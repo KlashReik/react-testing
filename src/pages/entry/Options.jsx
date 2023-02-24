573e7143e523c473033c92abfb9759bfcf5415e6
Ivan Bulavko <Ivan_Bulavko@epam.com>
2023-02-24 11:16:44 +0200

    shees

---
+++ src/pages/entry/Options.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ScoopOption from "./ScoopOption";
+import { Row } from "react-bootstrap";
+
+export default function Options({ optionType }) {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch((err) => console.log(err));
+  }, [optionType]);
+
+  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+
+  const optionItems = items.map((el) => (
+    <ItemComponent key={el.name} name={el.name} imagePath={el.imagePath} />
+  ));
+
+  return <Row>{optionItems}</Row>;
+}
