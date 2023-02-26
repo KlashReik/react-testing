bd5395d81c1a8a9e69c93760b26f26511095e522
Ivan Bulavko <Ivan_Bulavko@epam.com>
2023-02-26 20:15:48 +0200

    returned changes

--- src/context/OrderDetails.jsx
+++ src/context/OrderDetails.jsx
@@ -1,61 +1,43 @@
-import { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 import { pricePerItem } from "../constants";
 
-const OrderDetails = createContext();
+export const OrderDetails = createContext();
 
 // create custom hook to check whether we're in a provider
 export function useOrderDetails() {
   const contextValue = useContext(OrderDetails);
-
-  if (!contextValue) {
-    throw new Error(
-      "useOrderDetails must be called from within an OrderDetailsProvider"
-    );
-  }
+  if (!contextValue)
+    throw new Error("UseOrderDetails is out of OrderDetilsProvider");
 
   return contextValue;
 }
 
 export function OrderDetailsProvider(props) {
   const [optionCounts, setOptionCounts] = useState({
-    scoops: {}, // example: { Chocolate: 1, Vanilla: 2 }
-    toppings: {}, // example: { "Gummi Bears": 1 }
+    scoops: {},
+    toppings: {},
   });
 
   function updateItemCount(itemName, newItemCount, optionType) {
-    // make a copy of existing state
     const newOptionCounts = { ...optionCounts };
 
-    // update the copy with the new information
     newOptionCounts[optionType][itemName] = newItemCount;
 
-    // update the state with the updated copy
     setOptionCounts(newOptionCounts);
-
-    // alternate way using function argument to setOptionCounts
-    // see https://www.udemy.com/course/react-testing-library/learn/#questions/18721990/
-    // setOptionCounts((previousOptionCounts) => ({
-    //   ...previousOptionCounts,
-    //   [optionType]: {
-    //     ...previousOptionCounts[optionType],
-    //     [itemName]: newItemCount,
-    //   },
-    // }));
   }
 
   function resetOrder() {
     setOptionCounts({ scoops: {}, toppings: {} });
   }
 
-  // utility function to derive totals from optionCounts state value
   function calculateTotal(optionType) {
-    // get an array of counts for the option type (for example, [1, 2])
+    // get an array of counts for the option type
     const countsArray = Object.values(optionCounts[optionType]);
 
-    // total the values in the array of counts for the number of items
+    // total the values in the array of counts
+
     const totalCount = countsArray.reduce((total, value) => total + value, 0);
 
-    // multiply the total number of items by the price for this item type
     return totalCount * pricePerItem[optionType];
   }
 
@@ -65,5 +47,6 @@ export function OrderDetailsProvider(props) {
   };
 
   const value = { optionCounts, totals, updateItemCount, resetOrder };
+
   return <OrderDetails.Provider value={value} {...props} />;
 }
